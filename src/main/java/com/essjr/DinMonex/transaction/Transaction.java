80384be274a888fd347f6538ca507ea4f18f0d03
Evandro Sacramento II <evandro-dev@outlook.com>
2025-10-03 22:12:31 -0300

    [BACKEND] Create the Parcel entity and the relationship with Transaction.
    Fixes AB#373

--- src/main/java/com/essjr/DinMonex/transaction/Transaction.java
+++ src/main/java/com/essjr/DinMonex/transaction/Transaction.java
@@ -7,6 +7,8 @@ import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "transactions")
@@ -41,7 +43,33 @@ public class Transaction {
     @JoinColumn(name = "app_user_id", nullable = false)
     private AppUser appUser;
 
+
+    @OneToMany(mappedBy = "transaction", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Installment> installments = new ArrayList<>();
+
+
+    @Column(nullable = true)
+    private Integer totalInstallments;
+
+    @Column(nullable = true)
+    private Integer currentInstallment;
+
+
+
     public Transaction() {
+    }
+
+    public Transaction(Long id, String description, BigDecimal value, LocalDate dueDate, boolean isRecurring, TransactionType type, AppUser appUser, List<Installment> installments, Integer totalInstallments, Integer currentInstallment) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+        this.dueDate = dueDate;
+        this.isRecurring = isRecurring;
+        this.type = type;
+        this.appUser = appUser;
+        this.installments = installments;
+        this.totalInstallments = totalInstallments;
+        this.currentInstallment = currentInstallment;
     }
 
     public Transaction(Long id, String description, BigDecimal value, LocalDate dueDate, boolean isRecurring, TransactionType type, AppUser appUser) {
@@ -108,5 +136,29 @@ public class Transaction {
 
     public void setAppUser(AppUser appUser) {
         this.appUser = appUser;
+    }
+
+    public List<Installment> getInstallments() {
+        return installments;
+    }
+
+    public void setInstallments(List<Installment> installments) {
+        this.installments = installments;
+    }
+
+    public Integer getTotalInstallments() {
+        return totalInstallments;
+    }
+
+    public void setTotalInstallments(Integer totalInstallments) {
+        this.totalInstallments = totalInstallments;
+    }
+
+    public Integer getCurrentInstallment() {
+        return currentInstallment;
+    }
+
+    public void setCurrentInstallment(Integer currentInstallment) {
+        this.currentInstallment = currentInstallment;
     }
 }
